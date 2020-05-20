510608ed1063b580b0af896f57525daa8e3b9a62
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-20 01:48:11 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,4 @@
-class CostumeStores < ActiveRecord::Migration[5.0]
+class CreateCostumeStores < ActiveRecord::Migration[5.0]
   def change
     create_table :costume_stores do |t|
       t.string :name
