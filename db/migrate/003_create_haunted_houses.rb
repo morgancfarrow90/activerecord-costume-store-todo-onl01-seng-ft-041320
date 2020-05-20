4b8602a0dbc41948b63b7881ae22215c74e496b5
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-20 01:48:04 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,4 @@
-class HauntedHouses < ActiveRecord::Migration[5.0]
+class CreateHauntedHouses < ActiveRecord::Migration[5.0]
   def change
     create_table :haunted_houses do |t|
       t.string :name
